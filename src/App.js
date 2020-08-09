d441798ad150488eb0d63fee0c77598033c8e15f
its-pikesh <Pikesh.11606271@lpu.in>
2020-08-09 20:26:41 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useEffect } from "react";
+import "./App.css";
+import Login from "./Login";
+import Player from "./Player";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import { useDataLayerValue } from "./DataLayer";
+const spotify = new SpotifyWebApi();
+
+function App() {
+  const [{ user, token }, dispatch] = useDataLayerValue();
+  //Runcod based on a condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if (_token) {
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
+      });
+    }
+  }, [token]);
+
+  return (
+    <div className="app">
+      {token ? <Player spotify={spotify} /> : <Login />}
+    </div>
+  );
+}
+
+export default App;
